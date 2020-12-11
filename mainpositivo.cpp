e33b12977e4c37e51c9f729db43b643435dc4280
Yeimy <yei14@hotmail.com>
2020-12-11 18:28:45 -0500

    Se agrega .gitignore -Positivo.dev y mainpositivo.cpp

---
+++ mainpositivo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+int numero;
+	cout <<"Digite un numero  :";
+	cin >>numero;
+	
+	if (numero>0){
+		
+		cout <<"Este numero es positivo ";
+	
+}
+	else
+{      cout <<"Este numero es negativo";
+}
+	return 0;
+}
